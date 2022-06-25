12d88f1f0d82e7eaf6b92e0073deff609a155670
lucaspertile <lucaspts@gmail.com>
2022-06-25 17:03:02 -0300

    arqvuio teste github adicionado

---
+++ soma.js
@@ -0,0 +1,8 @@
+let a = 3
+let b = 3
+
+let resultado = a + b
+console.log(`O resultado é ${resultado}`);
+if(resultado<=5){
+    console.log("numero pequeno")
+}
---
+++ testeGitHub.js
@@ -0,0 +1,1 @@
+let testeGitHub = true
